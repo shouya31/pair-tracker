af61e4c4c53a7d7e2fdca1b8dc9f48e6e8e0e860
xmile0143 <shoya.higuchi0331@gmail.com>
2025-06-18 09:19:06 +0900

    Add Email VO in RegisterUserUseCase

--- src/application/user/usecases/RegisterUserUseCase.ts
+++ src/application/user/usecases/RegisterUserUseCase.ts
@@ -1,11 +1,16 @@
 import { IUserRepository } from '../../../domain/user/IUserRepository';
 import { User } from '../../../domain/user/User';
+import { Email } from '../../../domain/shared/Email';
 
 export class RegisterUserUseCase {
   constructor(private readonly userRepository: IUserRepository) {}
-
   async execute(name: string, email: string): Promise<void> {
-    const existingUser = await this.userRepository.findByEmail(User.create(name, email).getEmailVO());
+    if (!name.trim()) {
+      throw new Error('名前を入力してください');
+    }
+
+    const emailVO = Email.create(email);
+    const existingUser = await this.userRepository.findByEmail(emailVO);
     if (existingUser) {
       throw new Error('このメールアドレスは既に登録されています');
     }
