274ba629552a992a37715232b1a9bbda7688e944
xmile0143 <shoya.higuchi0331@gmail.com>
2025-06-21 20:21:01 +0900

    Refactor Inconsistent error class pattern and potential parameter issue

--- src/domain/user/errors/UserValidationError.ts
+++ src/domain/user/errors/UserValidationError.ts
@@ -1,13 +1,17 @@
-import { RequiredValueError, FormatError } from '../../shared/errors/ValidationError';
+import { ValidationError } from '../../shared/errors/ValidationError';
 
-export class UserNameRequiredError extends RequiredValueError {
+export class UserNameRequiredError extends ValidationError {
   constructor() {
-    super('ユーザー名');
+    super('名前を入力してください', 'ユーザー名', undefined);
   }
 }
 
-export class EmailFormatError extends FormatError {
+export class EmailFormatError extends ValidationError {
   constructor(value: string) {
-    super('メールアドレス', value, 'メールアドレス');
+    super(
+      'RFC 5321に準拠した有効なメールアドレスを入力してください',
+      'メールアドレス',
+      value
+    );
   }
 } 
